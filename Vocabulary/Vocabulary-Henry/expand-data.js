d57f67ed586ac777e4249f7939c2ef3a6fa2dc35
HenryWChang <hengrui0213@outlook.com>
2026-07-16 12:10:08 +0800

    Create expand-data.js

---
+++ Vocabulary/Vocabulary-Henry/expand-data.js
@@ -0,0 +1,39 @@
+// 💡 纯粹的 Expand 拓展数据，直接覆盖
+const henryExpandCustomWords = [
+    {
+        "word": "contradiction",
+        "meaning": "n. 矛盾，反驳",
+        "basic": "none",
+        "root": "dict-",
+        "prefix": "contra-",
+        "suffix": "-tion/-sion",
+        "root_m": "pref. 言，说 (例如: contradict, predict)",
+        "prefix_m": "pref. 反对，相反，相对 (例如: contradict, contrary)",
+        "suffix_m": "suff. 动作，状态 (例如: education, decision)（Suffix范例）",
+        "group": "opposition-logic"
+    },
+    {
+        "word": "antisocial",
+        "meaning": "adj. 反社会的",
+        "basic": "social",
+        "root": "none",
+        "prefix": "anti-",
+        "suffix": "-al",
+        "root_m": "none",
+        "prefix_m": "pref. 反对，相反 (例如: antisocial, antibody)",
+        "suffix_m": "suff. 具有...性质的 (例如: cultural, logical)",
+        "group": "opposition-logic"
+    },
+    {
+        "word": "biodegradable",
+        "meaning": "adj. 可生物降解的",
+        "basic": "none",
+        "root": "bio-",
+        "prefix": "de-",
+        "suffix": "-able/-ible",
+        "root_m": "pref. 生命，生物 (例如: biology, biography)",
+        "prefix_m": "pref. 向下，减少 (例如: decrease, devalue)（Prefix范例）",
+        "suffix_m": "suff. 能够...的，易于...的 (例如: understandable, visible)",
+        "group": "bio-process"
+    }
+];

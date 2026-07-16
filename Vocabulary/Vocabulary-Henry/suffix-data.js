29cd81778de4161831780ae80533aeba313696da
HenryWChang <hengrui0213@outlook.com>
2026-07-16 12:09:38 +0800

    Create suffix-data.js

---
+++ Vocabulary/Vocabulary-Henry/suffix-data.js
@@ -0,0 +1,6 @@
+// 💡 纯粹的 Suffix 后缀数据，直接覆盖
+const henrySuffixCustomWords = [
+    {"word": "-able/-ible", "meaning": "suff. 能够...的，易于...的 (例如: understandable, visible)"},
+    {"word": "-ize", "meaning": "suff. 使...化，变为 (例如: realize, modernize)"},
+    {"word": "-ment", "meaning": "suff. 行为，状态，结果 (role: development)"}
+];

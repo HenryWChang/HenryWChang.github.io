6c549f4ea4eaec37089dee6d6e229b854f6a1f65
HenryWChang <hengrui0213@outlook.com>
2026-07-16 12:08:47 +0800

    Create root-data.js

---
+++ Vocabulary/Vocabulary-Henry/root-data.js
@@ -0,0 +1,6 @@
+// 💡 纯粹的 Root 词根数据，直接覆盖
+const henryRootCustomWords = [
+    {"word": "bio-", "meaning": "pref. 生命，生物 (例如: biology, biography)"},
+    {"word": "chron-", "meaning": "pref. 时间 (例如: chronological, chronic)"},
+    {"word": "dict-", "meaning": "pref. 言，说 (例如: contradict, predict)"}
+];
